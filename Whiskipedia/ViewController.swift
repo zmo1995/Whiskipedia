26a8db3168699fb3b466681a3adb3e15dab91e6a
zhixuan mo <zmo1995@outlook.com>
2020-03-28 15:36:01 -0400

    Initial Commit

---
+++ Whiskipedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whiskipedia
+//
+//  Created by ZHI XUAN MO on 3/28/20.
+//  Copyright © 2020 zhixuan mo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
